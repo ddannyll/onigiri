672e06efa1b124f9fde6dd8c3fea2a50e55d463b
ddannyll <daniel.nguyen.s173@gmail.com>
2023-01-17 22:33:07 +1100

    dynamically import images

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,42 @@
+import { useState } from 'react';
+import './App.css'
+
+function importAll(r) {
+    let images = [];
+        r.keys().forEach((item) => { images.push(r(item))});
+    return images
+}
+
+const imagePaths = importAll(require.context('../sprites', false, /\.(png|jpe?g|svg)$/))
+
+
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    const NUM_IMAGES = 6
+    
+    const getImages = (numImages) => {
+        const gameImagesPaths = []
+        let imagePathsUnused = imagePaths
+        for (let i = 0; i < numImages; i++) {
+            const randomIndex = parseInt(Math.random() * imagePathsUnused.length)
+            gameImagesPaths.push(imagePathsUnused[randomIndex])
+            imagePathsUnused = imagePathsUnused.slice(0, randomIndex).concat(imagePathsUnused.slice(randomIndex + 1))
+        }
+        const gameImages = gameImagesPaths.map(imagePath => 
+            <img key={imagePath} src={imagePath} alt='gameImage'/>
+        )
+        return gameImages
+    }
+
+    const [state, setState] = useState(getImages(NUM_IMAGES)) 
+
+    return (
+        <div className="App">
+            {state}
+            <button onClick={() => {setState(getImages(NUM_IMAGES))}}>
+                Refresh!
+            </button>
+        </div>
+    );
 }
 
 export default App;
